3d429cf3f3a6d2a5a4f7e2d2c82c88246bb4b2cf
alikahwaji <ali.kahwaji@nmit.ac.nz>
2021-08-02 04:39:34 +1200

    Finish one matcher

--- project/calculator.spec.js
+++ project/calculator.spec.js
@@ -1,4 +1,4 @@
-xdescribe('calculator.js', function() {
+describe('calculator.js', function() {
   it('should add numbers to total', function () {
     const calculator = new Calculator()
     calculator.add(5)
@@ -30,4 +30,16 @@ xdescribe('calculator.js', function() {
 
     expect(calculator.total).toBe(20)
   })
+
+  it('should throw an error if total is not a number', function () {
+    const calculator = new Calculator()
+
+    expect(calculator.total).toBe(0)
+  })
+  it('has constructor', function () {
+    const calculator = new Calculator()
+    const calculator2 = new Calculator()
+
+    expect(calculator).toEqual(calculator2)
+    })
 })
